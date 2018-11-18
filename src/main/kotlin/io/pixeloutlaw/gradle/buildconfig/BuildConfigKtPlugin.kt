c79555ce80880657426fb2df228038e7e4351d60
Richard Harrah <topplethenunnery@gmail.com>
2018-11-17 21:16:49 -0500

    fixing tests by adding target.afterEvaluate wrapper

--- src/main/kotlin/io/pixeloutlaw/gradle/buildconfig/BuildConfigKtPlugin.kt
+++ src/main/kotlin/io/pixeloutlaw/gradle/buildconfig/BuildConfigKtPlugin.kt
@@ -6,15 +6,18 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 
 class BuildConfigKtPlugin : Plugin<Project> {
     override fun apply(target: Project) {
-        if (target.plugins.hasPlugin("org.jetbrains.kotlin.jvm")) {
-            target.logger.info("Registering generateBuildConfigKt")
-            target.tasks.register("generateBuildConfigKt", GenerateBuildConfigKtTask::class.java)
-            target.logger.info("Adding buildConfig generated directory to Kotlin src")
-            target.extensions.findByType(KotlinJvmProjectExtension::class.java)
-                ?.sourceSets
-                ?.findByName("main")
-                ?.kotlin
-                ?.srcDir(BuildConfigUtils.getFileOutputPath(target.buildDir.toPath(), target.group))
+        // afterEvaluate required to get access to plugin internals
+        target.afterEvaluate {
+            if (it.plugins.hasPlugin("org.jetbrains.kotlin.jvm")) {
+                it.logger.info("Registering generateBuildConfigKt")
+                it.tasks.register("generateBuildConfigKt", GenerateBuildConfigKtTask::class.java)
+                it.logger.info("Adding buildConfig generated directory to Kotlin src")
+                it.extensions.findByType(KotlinJvmProjectExtension::class.java)
+                    ?.sourceSets
+                    ?.findByName("main")
+                    ?.kotlin
+                    ?.srcDir(BuildConfigUtils.getFileOutputPath(it.buildDir.toPath(), it.group))
+            }
         }
     }
 }
