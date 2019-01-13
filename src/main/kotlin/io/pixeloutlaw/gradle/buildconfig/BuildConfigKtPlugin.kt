539f1f5f1055725c5cd278766627f6dc3f75df98
Richard Harrah <topplethenunnery@gmail.com>
2019-01-13 18:59:24 -0500

    update to use kotlin-plugin-dsl

--- src/main/kotlin/io/pixeloutlaw/gradle/buildconfig/BuildConfigKtPlugin.kt
+++ src/main/kotlin/io/pixeloutlaw/gradle/buildconfig/BuildConfigKtPlugin.kt
@@ -2,24 +2,23 @@ package io.pixeloutlaw.gradle.buildconfig
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.getting
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 class BuildConfigKtPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         // afterEvaluate required to get access to plugin internals
-        target.afterEvaluate { project ->
-            if (project.plugins.hasPlugin("org.jetbrains.kotlin.jvm")) {
-                project.logger.info("Registering generateBuildConfigKt")
-                project.tasks.register("generateBuildConfigKt", GenerateBuildConfigKtTask::class.java)
-                project.logger.info("Adding buildConfig generated directory to Kotlin src")
-                project.extensions.findByType(KotlinJvmProjectExtension::class.java)
-                    ?.sourceSets
-                    ?.findByName("main")
-                    ?.kotlin
-                    ?.srcDir(BuildConfigUtils.getRootOutputPath(project.buildDir.toPath()))
-                project.tasks.withType(KotlinCompile::class.java).configureEach { task ->
-                    task.dependsOn("generateBuildConfigKt")
+        target.afterEvaluate {
+            if (this.plugins.hasPlugin("org.jetbrains.kotlin.jvm")) {
+                this.logger.info("Registering generateBuildConfigKt task...")
+                this.tasks.register("generateBuildConfigKt", GenerateBuildConfigKtTask::class.java)
+                this.logger.info("Adding buildConfigKt generated directory to Kotlin src...")
+                this.extensions.findByType(KotlinJvmProjectExtension::class.java)
+                    ?.sourceSets?.findByName("main")
+                    ?.kotlin?.srcDir(BuildConfigUtils.getRootOutputPath(project.buildDir.toPath()))
+                this.tasks.getting(KotlinCompile::class) {
+                    dependsOn("generateBuildConfigKt")
                 }
             }
         }
