4508b3f1a834df8406acd8ca9b0100241160c126
Richard Harrah <topplethenunnery@gmail.com>
2019-01-14 18:54:13 -0500

    restructure extension

--- src/main/kotlin/io/pixeloutlaw/gradle/buildconfig/BuildConfigKtExtension.kt
+++ src/main/kotlin/io/pixeloutlaw/gradle/buildconfig/BuildConfigKtExtension.kt
@@ -1,23 +1,35 @@
 package io.pixeloutlaw.gradle.buildconfig
 
-import org.gradle.api.Project
+import io.pixeloutlaw.gradle.buildconfig.BuildConfigKtExtension.Companion.defaultAppName
+import io.pixeloutlaw.gradle.buildconfig.BuildConfigKtExtension.Companion.defaultClassName
+import io.pixeloutlaw.gradle.buildconfig.BuildConfigKtExtension.Companion.defaultPackageName
+import io.pixeloutlaw.gradle.buildconfig.BuildConfigKtExtension.Companion.defaultVersion
 
 /**
- * Configuration for the `generateBuildConfigKt` task. When created by the plugin,
- * the `appName`, `version`, and `packageName` properties are automatically determined
- * by the data available in the project itself.
+ * Configuration for the `generateBuildConfigKt` task.
+ *
+ * @property appName name of the project, defaults to [defaultAppName]
+ * @property version version of the project, defaults to [defaultVersion]
+ * @property packageName package name for generated build config file, defaults to [defaultPackageName]
+ * @property className class name for generated build config file, defaults to [defaultClassName]
  */
-open class BuildConfigKtExtension (
-    project: Project
-) {
-    var appName: String = ""
-    var version: String = ""
-    var packageName: String = ""
-    var className: String = "BuildConfig"
-
-    init {
-        appName = project.name
-        version = project.version.toString()
-        packageName = BuildConfigUtils.convertGavToPackageName(project.group, project.name)
+open class BuildConfigKtExtension {
+    /**
+     * Contains default values for the `generateBuildConfigKt` task.
+     * @property defaultAppName default app name, if [appName] equals this, the task will determine the value
+     * @property defaultVersion default version, if [version] equals this, the task will determine the value
+     * @property defaultPackageName default package name, if [packageName] equals this, the task will determine the value
+     * @property defaultClassName default class name
+     */
+    companion object {
+        const val defaultAppName = ""
+        const val defaultVersion = ""
+        const val defaultPackageName = ""
+        const val defaultClassName = "BuildConfig"
     }
+
+    var appName: String = defaultAppName
+    var version: String = defaultVersion
+    var packageName: String = defaultPackageName
+    var className: String = defaultClassName
 }
--- src/main/kotlin/io/pixeloutlaw/gradle/buildconfig/BuildConfigKtPlugin.kt
+++ src/main/kotlin/io/pixeloutlaw/gradle/buildconfig/BuildConfigKtPlugin.kt
@@ -10,10 +10,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 class BuildConfigKtPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         target.logger.info("Creating buildConfigKt extension...")
-        target.extensions.create<BuildConfigKtExtension>(
-            "buildConfigKt",
-            target
-        )
+        target.extensions.create<BuildConfigKtExtension>("buildConfigKt")
         target.logger.info("Registering generateBuildConfigKt task...")
         target.tasks.register("generateBuildConfigKt", GenerateBuildConfigKtTask::class.java)
         target.logger.info("Adding buildConfigKt generated directory to Kotlin src (if it exists)...")
