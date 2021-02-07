a7bc6272b597d39aa530893e513495fb678fe8fc
Richard Harrah <topplethenunnery@gmail.com>
2021-02-06 22:36:47 -0500

    update to pixeloutlaw-gradle-plugin 3.0.0

--- buildconfig-gradle-plugin/build.gradle.kts
+++ buildconfig-gradle-plugin/build.gradle.kts
@@ -1,9 +1,6 @@
 plugins {
     `kotlin-dsl`
-    `maven-publish`
-    id("org.jlleitschuh.gradle.ktlint")
-    id("io.gitlab.arturbosch.detekt")
-    id("org.jetbrains.dokka")
+    id("io.pixeloutlaw.gradle")
     id("com.gradle.plugin-publish")
 }
 
@@ -11,7 +8,7 @@ group = "io.pixeloutlaw.gradle"
 
 repositories {
     gradlePluginPortal()
-    jcenter()
+    mavenCentral()
 }
 
 dependencies {
@@ -22,10 +19,6 @@ dependencies {
 
     testImplementation("org.junit.jupiter:junit-jupiter:_")
     testImplementation("com.google.truth:truth:_")
-}
-
-detekt {
-    baseline = file("baseline.xml")
 }
 
 gradlePlugin {
@@ -39,25 +32,8 @@ gradlePlugin {
     }
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-    withJavadocJar()
-    withSourcesJar()
-}
-
 pluginBundle {
     website = "https://github.com/PixelOutlaw/buildconfig-gradle-plugin"
     vcsUrl = "https://github.com/PixelOutlaw/buildconfig-gradle-plugin"
     tags = listOf("kotlin", "build", "config", "buildconfig")
-}
-
-tasks.getByName("javadocJar", Jar::class) {
-    dependsOn("dokkaJavadoc")
-    from(buildDir.resolve("dokka/javadoc"))
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>() {
-    dependsOn("ktlintFormat")
-    kotlinOptions.jvmTarget = "1.8"
 }
