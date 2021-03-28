8a5ef252a9a4b81ad68d388ba0cce9ed6bca4ce8
Richard Harrah <topplethenunnery@gmail.com>
2021-03-28 16:35:33 -0400

    update to gradle 7.0-rc-1
    +semver: minor

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,10 @@
 plugins {
-    kotlin("jvm") version "1.4.30" apply false
-    id("io.pixeloutlaw.gradle") version "3.0.0"
+    kotlin("jvm") version "1.4.31" apply false
+    id("io.pixeloutlaw.gradle") version "4.2.2"
     id("io.pixeloutlaw.gradle.buildconfigkt") apply false
 }
 
 tasks.withType<Wrapper>().configureEach {
-    version = "6.8.2"
     doLast {
         copy {
             from(propertiesFile)
